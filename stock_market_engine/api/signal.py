bb13f0ad6b3f71f9dec971433b92bcf070790fdf
mattias <mattias.decharleroy@gmail.com>
2021-11-24 19:14:26 +0100

    Implement getSupportedSignalDetectors api

--- stock_market_engine/api/signal.py
+++ stock_market_engine/api/signal.py
@@ -2,7 +2,13 @@ import uuid
 
 from stock_market_engine.common import get_engine, store_engine, get_redis
 
+from stock_market.common.factory import Factory
+from stock_market.ext.signal import register_signal_detector_factories
+
 def register_signal_api(app):
+	factory = register_signal_detector_factories(Factory())
+
 	@app.get("/getsupportedsignaldetectors")
 	async def get_supported_signal_detectors():
-		pass
+		signal_detectors = factory.get_registered_names()
+		return [{'name' : sd, 'schema' : factory.get_schema(sd) } for sd in signal_detectors]
