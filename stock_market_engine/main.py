bf804056977e0118ec3e01a8de2b22c4762276aa
mattias <mattias.decharleroy@gmail.com>
2022-07-20 17:51:19 +0200

    Implement engine creation cache

--- stock_market_engine/main.py
+++ stock_market_engine/main.py
@@ -1,4 +1,5 @@
 import datetime
+import hashlib
 import uuid
 from http import HTTPStatus
 
@@ -27,8 +28,23 @@ async def startup_event():
     app.state.redis = init_redis_pool()
 
 
+def get_hash(engine_config):
+    json_config = engine_config.json()
+    return hashlib.md5(json_config.encode("utf-8")).hexdigest()
+
+
+async def get_cached_engine_id(engine_config, redis):
+    engine_id = await redis.get(get_hash(engine_config))
+    return engine_id
+
+
 @app.post("/create")
 async def create_engine(engine_config: EngineModel):
+    redis = get_redis(app)
+    engine_id = await get_cached_engine_id(engine_config, redis)
+    if engine_id is not None:
+        return engine_id
+
     engine = engine_config.create(
         get_stock_updater_factory(), get_signal_detector_factory()
     )
@@ -39,7 +55,8 @@ async def create_engine(engine_config: EngineModel):
 
     random_id = str(uuid.uuid4())
 
-    await store_engine(engine, random_id, get_redis(app))
+    await store_engine(engine, random_id, redis)
+    await redis.set(get_hash(engine_config), random_id)
     return random_id
 
 
